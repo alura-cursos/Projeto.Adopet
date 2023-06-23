0a25df0e517f642a9b776a7e3aca86c20c0e5910
bessax <andrebessax@gmail.com>
2023-06-23 10:45:47 -0300

    #2.1 - Duplicação de código - estado do projeto após o "faça como eu fiz".

--- Alura.Adopet.Console/Import.cs
+++ Alura.Adopet.Console/Import.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Alura.Adopet.Console
+﻿namespace Alura.Adopet.Console
 {
     internal class Import
     {
-        HttpClient client;
-        public Import()
-        {
-            this.client = ConfiguraHttpClient("http://localhost:5057");
-        }
         public async Task ImportacaoArquivoPetAsync(string caminhoDoArquivoDeImportacao)
-        {
-            
+        {            
             var leitor = new LeitorDeArquivo();
             List<Pet> listaDePet = leitor.RealizaLeitura(caminhoDoArquivoDeImportacao);
             foreach (var pet in listaDePet)
@@ -25,7 +11,8 @@ namespace Alura.Adopet.Console
                 System.Console.WriteLine(pet);
                 try
                 {
-                    var resposta = await CreatePetAsync(pet);
+                    var httpCreatePet = new HttpClientPet();
+                    await httpCreatePet.CreatePetAsync(pet);
                 }
                 catch (Exception ex)
                 {
@@ -33,25 +20,6 @@ namespace Alura.Adopet.Console
                 }
             }
             System.Console.WriteLine("Importação concluída!");
-        }
-
-        Task<HttpResponseMessage> CreatePetAsync(Pet pet)
-        {
-            HttpResponseMessage? response = null;
-            using (response = new HttpResponseMessage())
-            {
-                return client.PostAsJsonAsync("pet/add", pet);
-            }
-        }
-
-        HttpClient ConfiguraHttpClient(string url)
-        {
-            HttpClient _client = new HttpClient();
-            _client.DefaultRequestHeaders.Accept.Clear();
-            _client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            _client.BaseAddress = new Uri(url);
-            return _client;
-        }
+        }               
     }
 }
--- Alura.Adopet.Console/List.cs
+++ Alura.Adopet.Console/List.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Alura.Adopet.Console
+﻿namespace Alura.Adopet.Console
 {
     internal class List
     {
-        HttpClient client;
-        public List()
-        {
-            client = ConfiguraHttpClient("http://localhost:5057");
-        }
         public async Task ListaDadosPetsDaAPIAsync()
         {
-            IEnumerable<Pet>? pets = await ListPetsAsync();
+            var httpListPet = new HttpClientPet();
+            IEnumerable<Pet>? pets = await httpListPet.ListPetsAsync();
             System.Console.WriteLine("----- Lista de Pets importados no sistema -----");
             foreach (var pet in pets)
             {
@@ -25,20 +13,5 @@ namespace Alura.Adopet.Console
             }
         }
 
-        HttpClient ConfiguraHttpClient(string url)
-        {
-            HttpClient _client = new HttpClient();
-            _client.DefaultRequestHeaders.Accept.Clear();
-            _client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            _client.BaseAddress = new Uri(url);
-            return _client;
-        }
-
-        async Task<IEnumerable<Pet>?> ListPetsAsync()
-        {
-            HttpResponseMessage response = await client.GetAsync("pet/list");
-            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
-        }
     }
 }
