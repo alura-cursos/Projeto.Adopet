87201245ed020609589cd3aa8abad46e0c8eb817
bessax <andrebessax@gmail.com>
2023-06-29 11:26:53 -0300

    #gravação desafio 2

--- Alura.Adopet.Console/Util/PetAPartirDoCsv.cs
+++ Alura.Adopet.Console/Util/PetAPartirDoCsv.cs
@@ -10,12 +10,21 @@ namespace Alura.Adopet.Console.Util
     public static class PetAPartirDoCsv
     {
         public static Pet ConverteDoTexto(this string linha)
-        {     
-            string[]? propriedades = linha.Split(';');         
-            return new Pet(Guid.Parse(propriedades[0]),
-            propriedades[1],
-            int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro
-            );
+        {
+            string[]? propriedades = linha?.Split(';') ?? throw new ArgumentNullException("Texto não pode ser nulo!");
+
+            if (string.IsNullOrEmpty(linha)) throw new ArgumentException("Texto não pode ser vazio");
+
+            bool guidValido = Guid.TryParse(propriedades[0], out Guid petId);
+            if (!guidValido) throw new ArgumentException("Identificador do pet inválido!");
+
+            bool tipoValido = int.TryParse(propriedades[2], out int tipoPet);
+            if (!tipoValido) throw new ArgumentException("Tipo do pet inválido!");
+
+            int[] enums = Array.ConvertAll(Enum.GetValues<TipoPet>(), value => (int)value);
+            if (!enums.Contains(tipoPet)) throw new ArgumentException("Tipo do pet inválido!");
+
+            return new Pet(petId, propriedades[1], (TipoPet)tipoPet);
         }
     }
 }
--- Alura.Adopet.Testes/PetAPartirDoCSVTest.cs
+++ Alura.Adopet.Testes/PetAPartirDoCSVTest.cs
@@ -22,5 +22,45 @@ namespace Alura.Adopet.Testes
             //Assert
             Assert.NotNull(pet);
         }
+
+        [Fact]
+        public void QuandoStringForNulaDeveLancarArgumentNullException()
+        {
+            //Arrange
+            string? linha = null;
+
+            //Act + Assert
+            Assert.Throws<ArgumentNullException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoStringForVaziaDeveLancarArgumentException()
+        {
+            //Arrange
+            string? linha = string.Empty;
+
+            //Act + Assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoGuidForInvalidoDeveLancarArgumentException()
+        {
+            //Arrange
+            string? linha = "aksjdha;Nina;2";
+
+            //Act + Assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoTipoForInvalidoDeveLancarArgumentException()
+        {
+            //Arrange
+            string? linha = "609c9b0d-aa02-459f-a340-256513fc9bad;Nina;3";
+
+            //Act + Assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
+        }
     }
 }
