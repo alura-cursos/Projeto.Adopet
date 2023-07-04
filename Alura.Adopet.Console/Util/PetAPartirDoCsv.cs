c2ffade7f5ce0af908d066b11272c6fcd8535085
Daniel Portugal <dpcosta@gmail.com>
2023-07-04 09:08:46 -0300

    Resolução

--- Alura.Adopet.Console/Util/PetAPartirDoCsv.cs
+++ Alura.Adopet.Console/Util/PetAPartirDoCsv.cs
@@ -9,13 +9,23 @@ namespace Alura.Adopet.Console.Util
 {
     public static class PetAPartirDoCsv
     {
-        public static Pet ConverteDoTexto(this string linha)
+        public static Pet ConverteDoTexto(this string? linha)
         {     
-            string[]? propriedades = linha.Split(';');         
-            return new Pet(Guid.Parse(propriedades[0]),
-            propriedades[1],
-            int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro
-            );
+            string[]? propriedades = linha?.Split(';') ?? throw new ArgumentNullException("Texto não pode ser nulo!");
+
+            if (string.IsNullOrEmpty(linha)) throw new ArgumentException("Texto não pode ser vazio!");
+
+            if (propriedades.Length != 3) throw new ArgumentException("Texto inválido!");
+
+            bool sucesso = Guid.TryParse(propriedades[0], out Guid petId);
+            if (!sucesso) throw new ArgumentException("Guid inválido!");
+
+            sucesso = int.TryParse(propriedades[2], out int tipoPet);
+            if (!sucesso) throw new ArgumentException("Tipo de Pet inválido!");
+
+            if (tipoPet != 0 && tipoPet != 1) throw new ArgumentException("Tipo de Pet inválido!");
+
+            return new Pet(petId, propriedades[1], tipoPet == 0 ? TipoPet.Gato : TipoPet.Cachorro);
         }
     }
 }
--- Alura.Adopet.Testes/PetAPartirDoCSVTest.cs
+++ Alura.Adopet.Testes/PetAPartirDoCSVTest.cs
@@ -10,6 +10,7 @@ namespace Alura.Adopet.Testes
 {
     public class PetAPartirDoCsvTest
     {
+        // null, vazia, qtde insuficiente de campos, guid inválido, tipo inválido
         [Fact]
         public void QuandoStringForValidaDeveRetornatUmPet()
         {
@@ -21,6 +22,56 @@ namespace Alura.Adopet.Testes
 
             //Assert
             Assert.NotNull(pet);
+        }
+
+        [Fact]
+        public void QuandoStringNulaDeveLancarArgumentNullException()
+        {
+            // arrange
+            string? linha = null;
+
+            // act + assert
+            Assert.Throws<ArgumentNullException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoStringVaziaDeveLancarArgumentException()
+        {
+            // arrange
+            string? linha = string.Empty;
+
+            // act + assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoCamposInsuficientesDeveLancarArgumentException()
+        {
+            // arrange
+            string? linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão";
+
+            // act + assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoGuidInvalidoDeveLancarArgumentException()
+        {
+            // arrange
+            string? linha = "adkajhd;Lima Limão;1";
+
+            // act + assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoTipoInvalidoDeveLancarArgumentException()
+        {
+            // arrange
+           string? linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;2";
+
+            // act + assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
         }
     }
 }
